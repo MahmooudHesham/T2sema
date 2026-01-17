32c2d2e651277e1e93e871e8b9388b24536d78fc
Mahmoud Hesham <7odahesham4478@gmail.com>
2026-01-17 23:06:55 +0200

    feat: provide players cubit to home view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:t2sema/core/utils/service_locator.dart';
 import 'package:t2sema/features/history/presentation/views/history_view.dart';
 import 'package:t2sema/features/home/presentation/views/home_view.dart';
 import 'package:t2sema/features/match/presentation/views/generated_teams_view.dart';
 import 'package:t2sema/features/match/presentation/views/player_selection_view.dart';
+import 'package:t2sema/features/players/data/repos/players_repo.dart';
+import 'package:t2sema/features/players/presentation/manager/players/players_cubit.dart';
 
 abstract class AppRouter {
   static const kHome = '/home';
@@ -17,9 +21,11 @@ abstract class AppRouter {
     initialLocation: kHome,
     routes: [
       StatefulShellRoute.indexedStack(
-        builder: (context, state, navigationShell) {
-          return HomeView(navigationShell: navigationShell);
-        },
+        builder: (context, state, navigationShell) => BlocProvider(
+          create: (context) =>
+              PlayersCubit(getIt.get<PlayersRepo>())..fetchAllPlayers(),
+          child: HomeView(navigationShell: navigationShell),
+        ),
         branches: [
           // Home branch
           StatefulShellBranch(
