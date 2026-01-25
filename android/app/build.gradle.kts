477f09acf8bd56ec5d32baf3310bab261200340a
Mahmoud Hesham <7odahesham4478@gmail.com>
2026-01-25 14:26:59 +0200

    fix: fix flutter build apk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
@@ -35,10 +35,19 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation("com.squareup.okio:okio:2.10.0")
 }
---
+++ android/app/proguard-rules.pro
@@ -0,0 +1,8 @@
+# Ignore warnings about optional OkHttp dependencies
+-dontwarn org.bouncycastle.jsse.**
+-dontwarn org.conscrypt.**
+-dontwarn org.openjsse.**
+
+# Just in case, ignore these too if they appear
+-dontwarn okhttp3.**
+-dontwarn okio.**
